c07573dab9c878723b760dab98c22d3574f55741
Jinrui93 <60159216+Jinrui93@users.noreply.github.com>
2023-03-08 00:18:51 -0500

    Update streamlitApp_combined.py

--- streamlitApp_combined.py
+++ streamlitApp_combined.py
@@ -272,38 +272,3 @@ sleep = alt.Chart(subset).mark_circle(size=60).encode(
     )
 st.altair_chart(sleep, use_container_width=True)
 
-
-df = pd.read_csv('data.csv')
-df1 = pd.read_csv('data_month.csv')
-
-default_ym = 202003
-ym_slider = st.slider('Year_Month', min(df1['new_date']), max(df1['new_date']), value=default_ym)
-
-st.write("## Association between Insurance Rate and Number of Covid Cases")
-
-
-df5 = df1[df1['new_date'] == ym_slider]
-
-scatter = alt.Chart(df5).mark_circle().encode(
-    x=alt.X('percent_insured:Q', scale=alt.Scale(domain=[75, 100]), title = 'Percent Insured'),
-    y=alt.Y('cases_k:Q', title = 'Number of Cases (thousands)'),
-    tooltip=['state','percent_insured','cases_k']
-).properties(
-    title='Insured Rate and COVID-19 Cases'
-).interactive()
-
-scatter1 = alt.Chart(df5).mark_circle().encode(
-    x=alt.X('percent_insured:Q', scale=alt.Scale(domain=[75, 100]), title = 'Percent Insured'),
-    y=alt.Y('deaths_k:Q', title = 'Number of Deaths (thousands)'),
-    tooltip=['state','percent_insured','deaths_k']
-).properties(
-    title='Insured Rate and COVID-19 Deaths'
-).interactive()
-
-col1, col2 = st.columns(2)
-with col1:
-    st.altair_chart(scatter, use_container_width=True)
-
-with col2:
-    st.altair_chart(scatter1, use_container_width=True)
-    
